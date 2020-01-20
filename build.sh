5e601baa0acc61b172669b8c17c5167924e3b7bb
tany <tyy-28@163.com>
2020-01-20 23:14:16 +0800

    ch build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
 yum install nginx -y
-
+systemctl start nginx
+yum istall -y httpd-tools
 
